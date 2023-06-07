64152156f5f4ed6b6783f5e8dd269f40c3f9b8b1
Muhammad Humza <mh@afterflea.com>
2023-06-07 19:59:16 +0500

    create the unit test script for login, signup and authentication

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
+import unittest
+# from core.models import Message
+from room.models import Room,Message
 
-# Create your tests here.
+client = Client()
+user = User.objects.create_user(username='mp73', password='12345mp7xc')
+login = client.login(username='mp72', password='12345mp7xc')
